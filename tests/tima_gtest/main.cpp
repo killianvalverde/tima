6c4a317a3ee46844061d6927d4856b4e4fa95b36
Killian Valverde <KillianValverde@gmail.com>
2024-10-22 23:14:19 +0200

    test: add initial test files

---
+++ tests/tima_gtest/main.cpp
@@ -0,0 +1,34 @@
+/* tima
+ * Copyright (C) 2024 Killian Valverde.
+ *
+ * This file is part of tima.
+ *
+ * tima is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tima is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tima. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file        tima_gtest/main.cpp
+ * @brief       tima_gtest entry point.
+ * @author      Killian Valverde
+ * @date        2024/07/17
+ */
+
+#include <gtest/gtest.h>
+
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
